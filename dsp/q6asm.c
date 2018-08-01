69e1ef3cbaeafdcb15463fd9e518c31619174a68
Angelo G. Del Regno <kholk11@gmail.com>
2018-08-01 21:44:56 +0200

    techpack: audio: q6asm: Fix Q6 DSP comm and params for legacy SoC
    Legacy SoCs haven't got firmware updates, hence they understand
    their own version of the DSP structures, specifically:
    V2 - MSM8956, APQ8056
    V3 - MSM8996
    V4 - MSM8998, SDM630, SDM660 (SDM845 too)

--- dsp/q6asm.c
+++ dsp/q6asm.c
@@ -3245,13 +3245,16 @@ int q6asm_open_read_v4(struct audio_client *ac, uint32_t format,
 			uint16_t bits_per_sample, bool ts_mode,
 			uint32_t enc_cfg_id)
 {
-#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
-	return q6asm_open_read_v3(ac, format, bits_per_sample);
-#else
+	if (of_machine_is_compatible("qcom,msm8956") ||
+	    of_machine_is_compatible("qcom,apq8056"))
+		return q6asm_open_read_v2(ac, format, bits_per_sample);
+
+	if (of_machine_is_compatible("qcom,msm8996"))
+		return q6asm_open_read_v3(ac, format, bits_per_sample);
+
 	return __q6asm_open_read(ac, format, bits_per_sample,
 				 PCM_MEDIA_FORMAT_V4 /*media fmt block ver*/,
 				 ts_mode, enc_cfg_id);
-#endif
 }
 EXPORT_SYMBOL(q6asm_open_read_v4);
 
@@ -3267,13 +3270,16 @@ EXPORT_SYMBOL(q6asm_open_read_v4);
 int q6asm_open_read_v5(struct audio_client *ac, uint32_t format,
 			uint16_t bits_per_sample, bool ts_mode, uint32_t enc_cfg_id)
 {
-#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
-	return q6asm_open_read_v3(ac, format, bits_per_sample);
-#else
+	if (of_machine_is_compatible("qcom,msm8956") ||
+	    of_machine_is_compatible("qcom,apq8056"))
+		return q6asm_open_read_v2(ac, format, bits_per_sample);
+
+	if (of_machine_is_compatible("qcom,msm8996"))
+		return q6asm_open_read_v3(ac, format, bits_per_sample);
+
 	return __q6asm_open_read(ac, format, bits_per_sample,
 				 PCM_MEDIA_FORMAT_V5 /*media fmt block ver*/,
 				 ts_mode, enc_cfg_id);
-#endif
 }
 EXPORT_SYMBOL(q6asm_open_read_v5);
 
@@ -3602,13 +3608,17 @@ EXPORT_SYMBOL(q6asm_open_write_v3);
 int q6asm_open_write_v4(struct audio_client *ac, uint32_t format,
 			uint16_t bits_per_sample)
 {
-#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
-	return q6asm_open_write_v3(ac, format, bits_per_sample);
-#else
+	if (of_machine_is_compatible("qcom,msm8956") ||
+	    of_machine_is_compatible("qcom,apq8056"))
+		return q6asm_open_write_v2(ac, format, bits_per_sample);
+
+	if (of_machine_is_compatible("qcom,msm8996"))
+		return q6asm_open_write_v3(ac, format, bits_per_sample);
+
 	return __q6asm_open_write(ac, format, bits_per_sample,
 				  ac->stream_id, false /*gapless*/,
 				  PCM_MEDIA_FORMAT_V4 /*pcm_format_block_ver*/);
-#endif
+
 }
 EXPORT_SYMBOL(q6asm_open_write_v4);
 
@@ -3650,13 +3660,16 @@ EXPORT_SYMBOL(q6asm_stream_open_write_v3);
 int q6asm_open_write_v5(struct audio_client *ac, uint32_t format,
 			uint16_t bits_per_sample)
 {
-#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
-	return q6asm_open_read_v3(ac, format, bits_per_sample);
-#else
+	if (of_machine_is_compatible("qcom,msm8956") ||
+	    of_machine_is_compatible("qcom,apq8056"))
+		return q6asm_open_write_v2(ac, format, bits_per_sample);
+
+	if (of_machine_is_compatible("qcom,msm8996"))
+		return q6asm_open_write_v3(ac, format, bits_per_sample);
+
 	return __q6asm_open_write(ac, format, bits_per_sample,
 				  ac->stream_id, false /*gapless*/,
 				  PCM_MEDIA_FORMAT_V5 /*pcm_format_block_ver*/);
-#endif
 }
 EXPORT_SYMBOL(q6asm_open_write_v5);
 
@@ -3674,14 +3687,19 @@ int q6asm_stream_open_write_v4(struct audio_client *ac, uint32_t format,
 			       uint16_t bits_per_sample, int32_t stream_id,
 			       bool is_gapless_mode)
 {
-#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
-	return q6asm_stream_open_write_v3(ac, format, bits_per_sample,
+
+	if (of_machine_is_compatible("qcom,msm8956") ||
+	    of_machine_is_compatible("qcom,apq8056"))
+		return q6asm_stream_open_write_v2(ac, format, bits_per_sample,
 						stream_id, is_gapless_mode);
-#else
+
+	if (of_machine_is_compatible("qcom,msm8996"))
+		return q6asm_stream_open_write_v3(ac, format, bits_per_sample,
+						stream_id, is_gapless_mode);
+
 	return __q6asm_open_write(ac, format, bits_per_sample,
 				  stream_id, is_gapless_mode,
 				  PCM_MEDIA_FORMAT_V4 /*pcm_format_block_ver*/);
-#endif
 }
 EXPORT_SYMBOL(q6asm_stream_open_write_v4);
 
@@ -4496,6 +4514,10 @@ int q6asm_shared_io_free(struct audio_client *ac, int dir)
 }
 EXPORT_SYMBOL(q6asm_shared_io_free);
 
+int q6asm_enc_cfg_blk_pcm_v2(struct audio_client *ac,
+		uint32_t rate, uint32_t channels, uint16_t bits_per_sample,
+		bool use_default_chmap, bool use_back_flavor, u8 *channel_map);
+
 int q6asm_enc_cfg_blk_pcm_v3(struct audio_client *ac,
 			     uint32_t rate, uint32_t channels,
 			     uint16_t bits_per_sample, bool use_default_chmap,
@@ -5001,11 +5023,16 @@ static int q6asm_enc_cfg_blk_pcm_v5(struct audio_client *ac,
 	u32 frames_per_buf = 0;
 	int rc;
 
-#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
-	return q6asm_enc_cfg_blk_pcm_v3(ac, rate, channels, bits_per_sample,
+	if (of_machine_is_compatible("qcom,msm8956") ||
+	    of_machine_is_compatible("qcom,apq8056"))
+		return q6asm_enc_cfg_blk_pcm_v2(ac, rate, channels, bits_per_sample,
+					use_default_chmap, use_back_flavor,
+					channel_map);
+
+	if (of_machine_is_compatible("qcom,msm8996"))
+		return q6asm_enc_cfg_blk_pcm_v3(ac, rate, channels, bits_per_sample,
 					use_default_chmap, use_back_flavor,
 					channel_map, sample_word_size);
-#endif
 
 	if (!use_default_chmap && (channel_map == NULL)) {
 		pr_err("%s: No valid chan map and can't use default\n",
@@ -5510,6 +5537,14 @@ int q6asm_enc_cfg_blk_pcm_format_support_v4(struct audio_client *ac,
 					    uint16_t endianness,
 					    uint16_t mode)
 {
+	if (of_machine_is_compatible("qcom,msm8956") ||
+	    of_machine_is_compatible("qcom,apq8056"))
+		return __q6asm_enc_cfg_blk_pcm(ac, rate, channels, bits_per_sample);
+
+	if (of_machine_is_compatible("qcom,msm8996"))
+	 	return __q6asm_enc_cfg_blk_pcm_v3(ac, rate, channels,
+					   bits_per_sample, sample_word_size);
+
 	return __q6asm_enc_cfg_blk_pcm_v4(ac, rate, channels,
 					   bits_per_sample, sample_word_size,
 					   endianness, mode);
@@ -6264,12 +6299,17 @@ static int __q6asm_media_format_block_pcm_v3(struct audio_client *ac,
 		return -EINVAL;
 	}
 
-#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
-	return __q6asm_media_format_block_pcm_v3(ac, rate, channels,
+	if (of_machine_is_compatible("qcom,msm8956") ||
+	    of_machine_is_compatible("qcom,apq8056"))
+		return __q6asm_media_format_block_pcm(ac, rate, channels,
+					stream_id, bits_per_sample,
+					use_default_chmap, channel_map);
+
+	if (of_machine_is_compatible("qcom,msm8996"))
+		return __q6asm_media_format_block_pcm_v3(ac, rate, channels,
 					bits_per_sample, stream_id,
 					use_default_chmap, channel_map,
 					sample_word_size);
-#endif
 
 	pr_debug("%s: session[%d]rate[%d]ch[%d]bps[%d]wordsize[%d]\n", __func__,
 		 ac->session, rate, channels,
@@ -6459,11 +6499,16 @@ static int __q6asm_media_format_block_pcm_v5(struct audio_client *ac,
 		return -EINVAL;
 	}
 
-#if defined(CONFIG_ARCH_MSM8916) || defined(CONFIG_ARCH_MSM8996)
-	return __q6asm_media_format_block_multi_ch_pcm_v3(ac, rate, channels,
+	if (of_machine_is_compatible("qcom,msm8956") ||
+	    of_machine_is_compatible("qcom,apq8056"))
+		return __q6asm_media_format_block_multi_ch_pcm(ac, rate, channels,
+					use_default_chmap, channel_map,
+					bits_per_sample);
+
+	if (of_machine_is_compatible("qcom,msm8996"))
+		return __q6asm_media_format_block_multi_ch_pcm_v3(ac, rate, channels,
 					use_default_chmap, channel_map,
 					bits_per_sample, sample_word_size);
-#endif
 
 	pr_debug("%s: session[%d]rate[%d]ch[%d]bps[%d]wordsize[%d]\n", __func__,
 		 ac->session, rate, channels,
