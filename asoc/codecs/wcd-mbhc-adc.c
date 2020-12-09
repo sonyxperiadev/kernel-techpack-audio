adc4c72256fec0504101af16d768ec3d5acddb40
Meng Wang <mengw@codeaurora.org>
2020-12-09 12:33:01 +0800

    asoc: mbhc: add extcon device for MBHC
    Add extcon device for MBHC.
    
    Change-Id: I2f22223bc62f4f4b5f2e8cb61cf7e7d41548947f
    Signed-off-by: Meng Wang <mengw@codeaurora.org>
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- asoc/codecs/wcd-mbhc-adc.c
+++ asoc/codecs/wcd-mbhc-adc.c
@@ -1094,14 +1094,19 @@ static irqreturn_t wcd_mbhc_adc_hs_rem_irq(int irq, void *data)
 		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL, 0);
 		mbhc->btn_press_intr = false;
 		mbhc->is_btn_press = false;
-		if (mbhc->current_plug == MBHC_PLUG_TYPE_HEADSET)
+		if (mbhc->current_plug == MBHC_PLUG_TYPE_HEADSET) {
 			wcd_mbhc_report_plug(mbhc, 0, SND_JACK_HEADSET);
-		else if (mbhc->current_plug == MBHC_PLUG_TYPE_HEADPHONE)
+			extcon_set_state_sync(mbhc->extdev, EXTCON_JACK_MICROPHONE, 0);
+		} else if (mbhc->current_plug == MBHC_PLUG_TYPE_HEADPHONE) {
 			wcd_mbhc_report_plug(mbhc, 0, SND_JACK_HEADPHONE);
-		else if (mbhc->current_plug == MBHC_PLUG_TYPE_GND_MIC_SWAP)
+			extcon_set_state_sync(mbhc->extdev, EXTCON_JACK_HEADPHONE, 0);
+		} else if (mbhc->current_plug == MBHC_PLUG_TYPE_GND_MIC_SWAP) {
 			wcd_mbhc_report_plug(mbhc, 0, SND_JACK_UNSUPPORTED);
-		else if (mbhc->current_plug == MBHC_PLUG_TYPE_HIGH_HPH)
+			extcon_set_state_sync(mbhc->extdev, EXTCON_MECHANICAL, 0);
+		} else if (mbhc->current_plug == MBHC_PLUG_TYPE_HIGH_HPH) {
 			wcd_mbhc_report_plug(mbhc, 0, SND_JACK_LINEOUT);
+			extcon_set_state_sync(mbhc->extdev, EXTCON_JACK_LINE_OUT, 0);
+		}
 	} else {
 		/*
 		 * ADC COMPLETE and ELEC_REM interrupts are both enabled for
