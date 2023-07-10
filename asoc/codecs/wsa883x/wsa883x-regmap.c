27074b49a31a8a47f559b2c062069fa56ddf97e5
Ganapathiraju Sarath Varma <quic_ganavarm@quicinc.com>
2023-07-10 16:11:56 +0530

    asoc: codecs: wsa883x: update INTR mask & clear reg's as volatile
    Update Interrupt Mask and Clear registers as volatile
    to prevent audio mute during pdr recovery.
    As we observed these registers are been written during
    regcache sync which resulting in stopping the swrm
    interrupt. after updating these registers to volatile
    swrm interrupts are received as expected when there is
    any issue with PA on.
    
    Change-Id: I80bdf1556b1e07ec4751da5eb4614d30ad11f5a0
    Signed-off-by: Ganapathiraju Sarath Varma <quic_ganavarm@quicinc.com>

--- asoc/codecs/wsa883x/wsa883x-regmap.c
+++ asoc/codecs/wsa883x/wsa883x-regmap.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2015-2016, 2019, 2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/regmap.h>
@@ -382,6 +383,10 @@ static bool wsa883x_volatile_register(struct device *dev, unsigned int reg)
 {
 	if (reg <= WSA883X_BASE)
 		return 0;
+
+	if (reg == WSA883X_INTR_CLEAR0 || reg == WSA883X_INTR_CLEAR1 ||
+			reg == WSA883X_INTR_MASK0 || reg == WSA883X_INTR_MASK1)
+		return 1;
 
 	return ((wsa883x_reg_access[WSA883X_REG(reg)] & RD_REG) &&
 		!(wsa883x_reg_access[WSA883X_REG(reg)] & WR_REG));
